4b417a1e8e333d348403178df8727e53a0ef90c2
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-12-11 22:29:42 +0000

    added a new table and editted the program class.

--- src/ConsoleProgramForDotnet6.cs
+++ src/ConsoleProgramForDotnet6.cs
@@ -1,1 +1,3 @@
 Console.WriteLine("This is a new file added using branch one another text file");
+Console.WriteLine("Press enter to exit!");
+Console.ReadLine();
---
+++ src/MyTable.sql
@@ -0,0 +1,6 @@
+Create table
+(
+    ID INT Primary Key Identity(1,1),
+    Name NVARCHAR(100),
+    DoB DATETIME2
+);
