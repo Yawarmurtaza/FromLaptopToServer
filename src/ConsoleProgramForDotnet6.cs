a8e43a7b3a77d5ba155004e16ae0fe0b41b5881d
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-12-12 13:24:44 +0000

    fixed the conflict with remote main in line 1 of console program file.

--- src/ConsoleProgramForDotnet6.cs
+++ src/ConsoleProgramForDotnet6.cs
@@ -1,3 +1,3 @@
-Console.WriteLine("This is a new file added using branch one another text file - another commit to cause conflict!!");
+Console.WriteLine("This is a new file added using branch one another text file. FROM YAWAR 2 TO CAUSE CONFLICT WITH CHECKING LOCAL BRANCH WITH MAIN BRANCH.");
 Console.WriteLine("Press enter to exit!");
 Console.ReadLine();
