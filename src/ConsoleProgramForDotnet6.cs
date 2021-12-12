8a55cf11b75018d1b97f0a5dd2cfcf9a5c52fa6a
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-12-12 12:33:48 +0000

    edited the program.cs file top line to cause merge conflict

--- src/ConsoleProgramForDotnet6.cs
+++ src/ConsoleProgramForDotnet6.cs
@@ -1,3 +1,3 @@
-Console.WriteLine("This is a new file added using branch one another text file");
+Console.WriteLine("This is a new file added using branch one another text file - another commit to cause conflict!!");
 Console.WriteLine("Press enter to exit!");
 Console.ReadLine();
