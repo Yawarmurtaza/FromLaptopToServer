ed380aa653cb77d9a38ba90e252c8c981b249094
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-12-12 21:34:55 +0000

    added class and property comments.

--- src/Models/Refund.cs
+++ src/Models/Refund.cs
@@ -1,5 +1,7 @@
+// Defines a refund request.
 public class Refund
 {
+    // Get or sets the refund request id.
     public int Id {get;set;}
     public int CustomerId {get; set;}
     public int PayementId {get;set;}
