22e82ec4790fc7069b10b0ad60e5f3c5751f4a26
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-12-12 21:31:01 +0000

    added comments in the product class properties.

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -1,7 +1,12 @@
 // Defines a product class.
 public class Product
 {
+    // Gets or sets the product id.
     public int Id {get;set;}
+
+    // Gets or sets the product name.
     public string Name {get;set;}
+
+    // Gets or sets the category.
     public string Category {get;set;}
 }
