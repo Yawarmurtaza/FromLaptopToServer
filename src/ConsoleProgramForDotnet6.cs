f089d6529287fb8a50287bb54187504edf09da36
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-12-12 14:05:02 +0000

    added a comment in program class.

--- src/ConsoleProgramForDotnet6.cs
+++ src/ConsoleProgramForDotnet6.cs
@@ -1,3 +1,4 @@
+// this is .netnet 6 which allows us to skip the class program main code...
 Console.WriteLine("This is a new file added using branch one another text file. FROM YAWAR 2 TO CAUSE CONFLICT WITH CHECKING LOCAL BRANCH WITH MAIN BRANCH.");
 Console.WriteLine("Press enter to exit!");
 Console.ReadLine();
